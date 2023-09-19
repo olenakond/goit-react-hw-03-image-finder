c3377dbd7db9e991b2103b5d034105d054ee1353
Olena <shapovalev@gmail.com>
2023-09-19 10:40:39 +0200

    changes-zIndex

--- src/components/Modal/Modal.styled.js
+++ src/components/Modal/Modal.styled.js
@@ -11,7 +11,7 @@ export const Overlay = styled('div')(() => {
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: 'rgba(0, 0, 0, 0.8)',
-    Index: 1200,
+    zIndex: 1200,
   };
 });
 
